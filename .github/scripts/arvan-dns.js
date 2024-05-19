9ec4fa4e68a89e29ff156e84f8d8990883ec6e52
Ali Shabani <alishabani9270@gmail.com>
2024-05-19 13:27:45 +0330

    wip

--- .github/scripts/arvan-dns.js
+++ .github/scripts/arvan-dns.js
@@ -1,4 +1,34 @@
-const { DOMAIN, ARVAN_API_KEY } = process.env;
+const { DOMAIN, ARVAN_API_KEY, SERVER_IP } = process.env;
+
+const aRecords = ["app", "merchant", "moderation"];
+
+async function createARecord(name) {
+  return fetch(
+    `https://napi.arvancloud.ir/cdn/4.0/domains/${DOMAIN}/dns-records`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        type: "a",
+        value: [{ ip: SERVER_IP }],
+        ttl: 120,
+        upstream_https: "default",
+      }),
+    }
+  );
+}
+
+async function updateRecord(record) {
+  return fetch(
+    `https://napi.arvancloud.ir/cdn/4.0/domains/${DOMAIN}/dns-records/${record.id}`,
+    {
+      method: "PUT",
+      body: JSON.stringify({
+        value: [{ ip: SERVER_IP }],
+      }),
+    }
+  );
+}
 
 async function main() {
   const response = await fetch(
@@ -9,7 +39,24 @@ async function main() {
       },
     }
   ).then((res) => res.json());
-  console.log(response);
+
+  const exists = response.data.filter(
+    (record) =>
+      record.type.toLowercase() === "a" &&
+      aRecords.includes(record.name) &&
+      record.value[0].ip !== SERVER_IP
+  );
+
+  for (const record of exists) {
+    await updateRecord(record);
+  }
+
+  const submittedRecords = exists.map((record) => record.name);
+  const remaining = aRecords.filter((name) => !name.includes(submittedRecords));
+
+  for (const name of remaining) {
+    await createARecord(name);
+  }
 }
 
 main().then(() => {
