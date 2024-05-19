b0402357ced07449f8a8e87b1941ba77779b9a5d
Ali Shabani <alishabani9270@gmail.com>
2024-05-19 12:35:18 +0330

    wip

--- .github/scripts/arvan-dns.js
+++ .github/scripts/arvan-dns.js
@@ -2,7 +2,12 @@ const { DOMAIN, ARVAN_API_KEY } = process.env;
 
 async function main() {
   const response = await fetch(
-    `https://napi.arvancloud.ir/cdn/4.0/domains/${DOMAIN}/dns-records`
+    `https://napi.arvancloud.ir/cdn/4.0/domains/${DOMAIN}/dns-records`,
+    {
+      headers: {
+        Authorization: `${ARVAN_API_KEY}`,
+      },
+    }
   ).then((res) => res.json());
   console.log(response);
 }
